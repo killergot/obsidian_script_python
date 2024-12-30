762c26e482490a65dd34912f30b4652c29a0ae13
killergot <disney.132435morozko@mail.ru>
2024-12-30 04:14:13 +0300

    fix: add \filename in param, not in argv

--- src/FileClasses/MainActivity.py
+++ src/FileClasses/MainActivity.py
@@ -1,5 +1,7 @@
-from src.FileClasses.Searcher import SearcherAllFiles,os
+from src.FileClasses.Searcher import SearcherAllFiles, os
 from src.FileClasses.FileSetter import FileSetter
+
+
 class MainActivity:
     help = """
     Можно скопировать или перенести файл и все его подфайлы в другое место.
@@ -7,28 +9,29 @@ class MainActivity:
     -s/--src <path> - Главный файл
     --dst <path> - Целевая папка
     Если вы не включаете интерактивный режим, то вышеуказанные флаги обязательны
-    
+
     --del - Указывает, что нужно именно перенести файл
     --folder - Указывает, что нужно создавать папки и делать как в источнике
 
     --interactive - Включить интерактивный режим
         - Данный флаг пока не работает..:)
     """
-    def __init__(self,args):
+
+    def __init__(self, args):
         if len(args) < 3:
             print(self.help)
             print(args)
         else:
             for i in range(len(args)):
                 if args[i] == '-s' or args[i] == '--src':
-                    self.src = args[i+1]
+                    self.src = args[i + 1]
                 if args[i] == '--dst':
-                    self.dst = args[i+1]
+                    self.dst = args[i + 1]
                 if args[i] == '--del':
                     self.del_flag = True
                 if args[i] == '--folder':
                     self.folder_flag = True
-            if getattr(self,'src',None) is None or getattr(self,'dst',None) is None:
+            if getattr(self, 'src', None) is None or getattr(self, 'dst', None) is None:
                 print(self.help)
                 print(args)
             else:
@@ -36,10 +39,10 @@ class MainActivity:
 
     def main(self):
         searcher = SearcherAllFiles()
-        file_path: str = self.src + r'\тест.md'
+        file_path: str = self.src
         links = searcher.searchIn(file_path)
         print(links)
         FileSetter.file_transfer(links,
                                  self.dst,
-                                 del_flag=getattr(self,'del_flag',False),
-                                 folder_flag=getattr(self,'folder_flag',False))
+                                 del_flag=getattr(self, 'del_flag', False),
+                                 folder_flag=getattr(self, 'folder_flag', False))
